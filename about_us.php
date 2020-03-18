baec30888f4b66dfd2dcc4e85f8e54f749e090ed
Guillaume CAVALIE <actix2013@gmail.com>
2020-03-18 21:20:57 +0100

    fin des includes dans about_us, debut travail  csv

--- about_us.php
+++ about_us.php
@@ -114,86 +114,10 @@
 
           </main>
     <footer>
-        <section class="items">
-            <article class="hidden">
-                <a href="plateformes.html">
-                    <div>Platforms</div>
-                </a>
-            </article>
-            <article class="hidden">
-                <a href="produits.html">
-                    <div>Products</div>
-                </a>
-            </article>
-            <article class="hidden">
-                <a href="about_us.php">
-                    <div>About Us</div>
-                </a>
-            </article>
-            <article class="hidden">
-                <a href="index.html">
-                    <div class="logo_retro_invaders">
-                        <img class="logo_desktop" alt="logo_retro_invaders_desktop" src="Images/Autres/blog-loco-green.png" height="150" width="auto">
-                    </div>
-                </a>
-            </article>
-            <article class="hidden">
-                <a href="contact.html">
-                    <div>Contact</div>
-                </a>
-            </article>
-            <article>
-                <div>FAQ</div>
-            </article>
-
-            <article>
-                <div>Legal Mentions</div>
-            </article>
-        </section>
-
-        <section class="logo-rs-footer-container">
-            <article>
-                <a href="https://www.instagram.com/?hl=fr">
-                   <img class="logo_instagram" alt="logo_instagram" src="Images/Autres/instagram-green.png" height="40" width="auto">
-                </a>
-            </article>
-            
-            <article>
-                <a href="https://twitter.com/?lang=fr">
-                    <img alt="logo_twitter" src="Images/Autres/twitter-green.png" height="40" width="auto">
-                </a>
-            </article>
-
-            <article>
-                <a href="https://fr-fr.facebook.com/">
-                    <img alt="logo_facebook" src="Images/Autres/facebook-green.png" height="40" width="auto">
-                </a>
-            </article>
-
-            <article>
-                <a href="https://www.pinterest.fr/">
-                    <img alt="logo_pinterest" src="Images/Autres/pinterest-green.png" height="40" width="auto">
-                </a>
-            </article>
-
-        </section>
+        <?php require_once 'includes/_footer.html';?>
     </footer>
         
-        <script type="text/javascript">
-        jQuery(function(){
-          var div = jQuery("#burger-menu-click"), div = jQuery("#nav-mobile-container"), div = jQuery(".close-menu-mobile");
-          jQuery("#burger-menu-click").click(function(){
-            jQuery("#burger-menu-click").toggleClass ("no-content");
-            jQuery("#nav-mobile-container").toggleClass ("no-content");
-            jQuery(".close-menu-mobile").toggleClass ("no-content");
-          });     
-          jQuery(".close-menu-mobile").click(function(){
-            jQuery("#burger-menu-click").toggleClass ("no-content");
-            jQuery("#nav-mobile-container").toggleClass ("no-content");
-            jQuery(".close-menu-mobile").toggleClass ("no-content");
-          });   
-      });
-        </script>
+       <?php require_once 'includes/_burger_menu_click.js';?>
           
           
       </div>
