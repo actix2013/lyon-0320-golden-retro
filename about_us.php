73e28944ef35f4f0be45e49375feb86339181e0a
Guillaume CAVALIE <actix2013@gmail.com>
2020-03-18 21:08:34 +0100

    include php pour head de about us

--- about_us.php
+++ about_us.php
@@ -1,21 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Retro Invader</title>
-  <meta lang="en">
-  <meta name="title" content="Retro Invaders">
-  <meta charset="UTF-8">
-  <meta name="description" content="Retro Invaders helps you to find your favorite retro games">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <link rel="stylesheet" type="text/css" href="css/about_us_style.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900|Roboto:300,400,700,900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/header_style.css" />
-  <link rel="stylesheet" type="text/css" href="src/_footer.css" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-
+    <?php require_once 'includes/_head_about_us.html';?>
   </head>
   <body>
 
