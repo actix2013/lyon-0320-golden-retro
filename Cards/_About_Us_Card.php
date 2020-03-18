24ae041d4b2f437833d09c1d96574b656b2ecf60
Guillaume CAVALIE <actix2013@gmail.com>
2020-03-18 23:17:28 +0100

    about_us php ok  commit 2 final pour merge dans Dev et master

--- Cards/_About_Us_Card.php
+++ Cards/_About_Us_Card.php
@@ -3,17 +3,14 @@ $csv = array_map('str_getcsv', file('data/about_us.csv'));
 for($i =1 ;$i < count($csv) ;$i++ ){ ?>
 
     <div class="gca_row ">
-        <div class="gca_img_box guillaume">   </div>
-
+        <div class="<?php echo  $csv[$i][4] ?> >">   </div>
         <div class="gca_column">
             <span class="pseudo"><?php echo $csv[$i][0]; ?></span>
 
             <div class="gca_content">
                 <span class="category"><span class="category-title">Favorite platform: </span><?php echo $csv[$i][1]; ?></span>
-                <span class="category"><span class="category-title">Favorite game: </span>Diablo</span>
                 <span class="category"><span class="category-title">Favorite game: </span><?php echo $csv[$i][2]; ?></span>
                 <p>
-
                     <?php echo $csv[$i][3];?>
                 </p>
             </div>
